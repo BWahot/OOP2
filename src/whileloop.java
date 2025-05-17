d8fff1b98e0485b6132e9f03378a3be5ef8dec5f
BWahot <benjaminwahothi04@gmail.com>
2025-05-17 18:48:27 +0300

    A commit of the java assignment

---
+++ src/whileloop.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class whileloop {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int maximum=scanner.nextInt();
+        int x=1;
+        while(x<=5) {
+            System.out.println( "x:"+ x);
+            x++;
+        }
+        scanner.close();
+    }
+}
